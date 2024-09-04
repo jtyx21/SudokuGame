8be2f097699b19987a87df62aa7a0af83d8ffc06
jayden <jaydentanyx@gmailcom>
2024-09-04 20:36:39 +0800

    added incompleted timer

--- index.py
+++ index.py
@@ -1,7 +1,33 @@
 import numpy as np
+import time 
+import threading
 
-
+mode = 0
 checkboard = np.arange(1,10,dtype=object).reshape(3,3)
+
+def choose_difficulty():
+    global mode
+    user_difficulty = str(input("Choose your difficulty (Easy,Medium,Hard)"))
+    if user_difficulty.capitalize() == "Easy":
+        mode = 20
+    elif user_difficulty.capitalize() == "Medium":
+        mode = 15
+    elif user_difficulty.capitalize() == "Hard":
+        mode = 7
+    else:
+        print("Please select an approriate difficulty")
+        choose_difficulty()
+
+
+def countdown():
+    global mode
+
+    for i in range(mode):
+        mode -= 1
+        time.sleep(1)
+        
+
+    print("Out of time!")
 
 
 def swap(shuffles):
@@ -15,6 +41,7 @@ def swap(shuffles):
     return checkboard
 
 def game_start():
+    global mode
     checkboard = np.arange(1,10,dtype=object).reshape(3,3)
     checkboard = swap(10)
     random_space_row = np.random.randint(3)
@@ -22,17 +49,20 @@ def game_start():
     random_space = checkboard[random_space_row,random_space_column]
     checkboard[random_space_row,random_space_column] = '?'
     print(checkboard)
-    check_ans = True
-    while check_ans:
+    while mode >0:
+        print(str(mode) + "s")
         user_ans = int(input("Number:"))
         if user_ans == random_space:
             print("Correct!")
-            check_ans = False
             check_continue()
         else:
             print("Try again")
             continue
 
+
+        if mode == 0:
+            break
+        
         
 
 def check_continue():
@@ -46,4 +76,11 @@ def check_continue():
         print("Please key in yes or no")
         check_continue()
 
+
+
+
+choose_difficulty()
+
+countdown_thread = threading.Thread(target=countdown)
+countdown_thread.start()
 game_start()
